6c09c98b044afaa7f48d6409241c3030d9685dd7
Ameenbro123 <81590743+Ameenbro123@users.noreply.github.com>
2022-01-24 02:22:26 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-cat "Would you like to install common tools? [Y for yes, N for no]"
-read yorn
-if [$yorn == "Y"]
-then
+read -p "Would you like to install common tools? [Y for yes, N for no]: " yorn
+if [ $yorn = "Y" ]; then
     sudo apt-get update
     sudo apt install python python3 python3-pip python-pip veil git curl rkhunter aide
     mkdir installs
-    git clone https://github.com/Screetsec/TheFatRat.git -C ./installs/
+    git clone https://github.com/Screetsec/TheFatRat.git ./installs/
     cd TheFatRat
     chmod +x setup.sh && ./setup.sh
 else
     clear
-    cat Bye.
-    sleep 4
+    echo Bye.
+    sleep 5
     clear
 fi
