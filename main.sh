9953236e0af3e70e119b52aaa8a58ca770a3b16c
Ameenbro123 <81590743+Ameenbro123@users.noreply.github.com>
2022-02-25 23:39:45 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-read -p "Would you like to install common tools? [Y for yes, N for no]: " yorn
-if [ $yorn = "Y" ]; then
-    sudo apt-get update
-    sudo apt install python python3 python3-pip python-pip git curl rkhunter aide
-    mkdir installs
-    git clone https://github.com/Screetsec/TheFatRat.git ./installs/
-    cd ./installs/TheFatRat
-    chmod +x setup.sh && ./setup.sh
-else
-    clear
-    echo Bye.
-    sleep 5
-    clear
-fi
+sudo apt-get update
+sudo apt install python python3 python3-pip python-pip git curl rkhunter aide ufw cmatrix
+sudo systemctl enable ufw
+sudo ufw enable
+clear
+echo "UFW Firewall is enabled, to ssh or rdp into this machine you must disable or open a port"
+sleep 5
